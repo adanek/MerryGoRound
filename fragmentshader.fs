7b50394d0eb2d55edc80e7bec7d8bf4333411571
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-06-05 11:51:48 +0200

    lighting

--- fragmentshader.fs
+++ fragmentshader.fs
@@ -4,6 +4,10 @@ uniform vec3 EyeDirection;
 uniform vec3 Attenuation;
 uniform float ambient;
 
+uniform int SHOW_AMBIENT;
+uniform int SHOW_DIFFUSE;
+uniform int SHOW_SPECULAR;
+
 uniform vec3 L1_Color;
 uniform vec3 L1_Position;
 
@@ -21,29 +25,53 @@ out vec4 FragColor;
 
 void main()
 {   
-    vec3 lightDirection = LightPosition - vec3(vPosition);
-    float lightDistance = length(lightDirection);
+   
+     vec3 scatteredLight = vec3(0);
+     vec3 reflectedLight = vec3(0);
 
-    // Normalize lightdirection
+    if(SHOW_AMBIENT == 1){
+        scatteredLight = scatteredLight + vec3(ambient);
+    }
+
+    // Lightsource 1
+
+    vec3 sL = vec3(0);
+    vec3 r1 = vec3(0);
+
+    vec3 v = normalize(EyeDirection);
+    vec3 n  =normalize(vNormal);
+
+    // Set LS1
+    vec3 lpos = L1_Position;
+    vec3 lcol = L1_Color;
+    
+
+    vec3 lightDirection = lpos - vec3(vPosition);
+    float lightDistance = length(lightDirection);    
     lightDirection = lightDirection / lightDistance;
 
-    vec3 l = normalize(LightDirection);
-	vec3 v = normalize(EyeDirection);
-    vec3 h = normalize(lightDirection + v);
+    // Calculate attenuation
+     float attenuation = 1.0 / (Attenuation.x + Attenuation.y * lightDistance + Attenuation.z * lightDistance * lightDistance);
 
-    float attenuation = 1.0 / (Attenuation.x + Attenuation.y * lightDistance + Attenuation.z * lightDistance * lightDistance);
+    // Calculate halfvector
+    vec3 h = normalize(lightDirection + v);   
 
-	float diffuse = max(0.0, dot(vNormal, l));    
-	float specular = max(0.0, dot(vNormal, h));
-	
-	if(diffuse == 0.0)
-		specular = 0.0;
-	else
-		specular = pow(specular, Shininess);
-	
-    vec3 scatteredLight = vec3(ambient) + LightColor * diffuse * attenuation;
-    vec3 reflectedLight = LightColor * specular * Strength * attenuation;
+
+	float diffuse = max(0, dot(n, lightDirection));    
+	float specular = max(0, dot(n, h));	
     
+	if(diffuse == 0.0){
+        specular = 0.0;
+    } else {
+    	specular = pow(specular, Shininess);
+    }	
+	
+
+    scatteredLight = vec3(ambient) + lcol * diffuse * attenuation;
+    reflectedLight = lcol * specular * Strength * attenuation;
+    
+
+
     vec3 rgb = min(vColor.rgb * scatteredLight + reflectedLight, vec3(1.0));
     FragColor = vec4(rgb, vColor.a);
 }
