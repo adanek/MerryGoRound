ed9f64235a90f8177b79bc704ba3ccc227cb3dc3
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-06-05 10:45:09 +0200

    test

--- fragmentshader.fs
+++ fragmentshader.fs
@@ -1,12 +1,17 @@
 #version 330
 
-uniform vec3 LightColor;
-uniform vec3 LightDirection;
-uniform vec3 LightPosition;
 uniform vec3 EyeDirection;
+uniform vec3 Attenuation;
+uniform float ambient;
+
+uniform vec3 L1_Color;
+uniform vec3 L1_Position;
+
+uniform vec3 L2_Color;
+uniform vec3 L2_Position;
+
 uniform float Shininess;
 uniform float Strength;
-uniform float ambient;
 
 in vec4 vColor;
 in vec3 vNormal;
@@ -15,8 +20,7 @@ in vec4 vPosition;
 out vec4 FragColor;
 
 void main()
-{
-    
+{   
     vec3 lightDirection = LightPosition - vec3(vPosition);
     float lightDistance = length(lightDirection);
 
@@ -27,6 +31,8 @@ void main()
 	vec3 v = normalize(EyeDirection);
     vec3 h = normalize(lightDirection + v);
 
+    float attenuation = 1.0 / (Attenuation.x + Attenuation.y * lightDistance + Attenuation.z * lightDistance * lightDistance);
+
 	float diffuse = max(0.0, dot(vNormal, l));    
 	float specular = max(0.0, dot(vNormal, h));
 	
@@ -35,8 +41,8 @@ void main()
 	else
 		specular = pow(specular, Shininess);
 	
-    vec3 scatteredLight = vec3(ambient) + LightColor * diffuse;
-    vec3 reflectedLight = LightColor * specular * Strength;
+    vec3 scatteredLight = vec3(ambient) + LightColor * diffuse * attenuation;
+    vec3 reflectedLight = LightColor * specular * Strength * attenuation;
     
     vec3 rgb = min(vColor.rgb * scatteredLight + reflectedLight, vec3(1.0));
     FragColor = vec4(rgb, vColor.a);
